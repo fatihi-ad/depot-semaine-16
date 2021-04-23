03efc816b5db0fcc67d3b6b472c890e6bf3ded61
fatihi-ad <74970085+fatihi-ad@users.noreply.github.com>
2021-04-23 12:06:09 +0200

    Add files via upload
    exercice formulaire PDO

---
+++ Inserer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <title>Page d'acceuil</title>
+
+        </head>
+
+        <body> 
+
+        <a href="insertion.php">Inserer</a>
+
+        </tbody>
+
+    </body>
+</html>
